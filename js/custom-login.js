522bd862d3f60f12c3620b1c148506a7a4ec430e
Tali Allison <alexis.langager.remote@gmail.com>
2026-04-17 12:44:12 -0500

    changed code

--- js/custom-login.js
+++ js/custom-login.js
@@ -1,1 +1,25 @@
 console.log("hello");
+
+const container = document.querySelector(".container");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const data = new FormData(form);
+    const dataObj = Object.fromEntries(data.entries());
+
+    let name = dataObj.name;
+    let email = dataObj.email;
+    let password = dataObj.password;
+
+    let message = `Welcome, ${name}! You are logged in!`;
+
+    let endParagraph = document.createElement("p");
+    endParagraph.textContent = message;
+
+    container.appendChild(endParagraph);
+
+    form.reset();
+    form.style.display = "none";
+});
